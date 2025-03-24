81c1f2e902e9f59ab5df0955b6b1fac188d859a8
SethupathyD <sethupathy.d@syncfusion.com>
2025-03-24 18:31:31 +0530

    Code updated

---
+++ TapGesture/TapGesture/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TapGesture
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TapGesture/TapGesture/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TapGesture
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
